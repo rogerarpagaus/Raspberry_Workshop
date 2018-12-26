94d427fff35595d82c4f980ba503a37897b27a18
rogerarpagaus <46073270+rogerarpagaus@users.noreply.github.com>
2018-12-26 17:22:00 +0100

    Update LEDon-off-fast-50time1.py

--- LEDon-off-fast-50time1.py
+++ LEDon-off-fast-50time1.py
@@ -4,7 +4,7 @@ gpio.setmode(gpio.BCM)
 gpio.setup(18, gpio.OUT)
 for i in range (1,50):
 	gpio.output(18, gpio.HIGH)
-	print(i,“ On“)	
+	print(i,' On')	
 	time.sleep(0.1)
 	gpio.output(18, gpio.LOW)
 	print(i,“ Off“)
