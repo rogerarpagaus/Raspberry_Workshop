d945eb8e9eba9dd57975b8b1dba8beebc6cf35d0
rogerarpagaus <46073270+rogerarpagaus@users.noreply.github.com>
2018-12-26 17:18:56 +0100

    Create LEDon-off-fast-50time1.py

---
+++ LEDon-off-fast-50time1.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as gpio
+import time
+gpio.setmode(gpio.BCM)
+gpio.setup(18, gpio.OUT)
+for i in range (1,50):
+	gpio.output(18, gpio.HIGH)
+	print(i,“ On“)	
+	time.sleep(0.1)
+	gpio.output(18, gpio.LOW)
+	print(i,“ Off“)
+	time.sleep(0.1)
+gpio.output(18, gpio.LOW)
+gpio.cleanup()
+Print(„End“)
