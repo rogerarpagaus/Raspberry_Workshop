1298017cf27b66053962b1a27937f539173af71c
rogerarpagaus <46073270+rogerarpagaus@users.noreply.github.com>
2018-12-26 18:03:23 +0100

    Update LEDon-off-fast-50time1.py

--- LEDon-off-fast-50time1.py
+++ LEDon-off-fast-50time1.py
@@ -1,3 +1,4 @@
+#Let the LED go on/off 50 times, each status should stay for 30ms.
 import RPi.GPIO as gpio
 import time
 gpio.setmode(gpio.BCM)
@@ -5,10 +6,10 @@ gpio.setup(18, gpio.OUT)
 for i in range (1,50):
 	gpio.output(18, gpio.HIGH)
 	print(i,' On')	
-	time.sleep(0.05)
+	time.sleep(0.03)
 	gpio.output(18, gpio.LOW)
 	print(i,' Off')
-	time.sleep(0.05)
+	time.sleep(0.03)
 gpio.output(18, gpio.LOW)
 gpio.cleanup()
 Print('End')
