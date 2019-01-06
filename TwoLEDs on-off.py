ca7100e72866027b48415badf7e17b493d2a5eab
Roger Arpagaus <rogerarpagaus@ymail.com>
2019-01-06 17:47:50 +0100

    New Code for switching two LEDs on / off based on a question.

---
+++ TwoLEDs on-off.py
@@ -0,0 +1,21 @@
+# Code for switching two LEDs on/off based on a selection, one on Pin 18 and one on Pin 23.
+
+import RPi.GPIO as GPIO
+import time
+
+gpio.setmode(gpio.BCM)
+gpio.setmode(18, gpio.OUT)
+gpio.setmode(23, gpio.OUT)
+
+color = input ("which color should be on, yellow or green")
+if color == "yellow":
+  gpio.output(23, gpio.HIGH)
+  time.sleep(2)
+  gpio.output(23, gpio.LOW)
+
+if color == "green":
+  gpio.output(18, gpio.HIGH)
+  time.sleep(2)
+  gpio.output(18, gpio.LOW)
+
+gpio.cleanup()
