96133199216878b0b8c992e1a10aa374d7c097c7
Roger Arpagaus <rogerarpagaus@ymail.com>
2019-01-06 17:55:55 +0100

    Code correction

--- TwoLEDs-on-off.py
+++ TwoLEDs-on-off.py
@@ -4,8 +4,8 @@ import RPi.GPIO as gpio
 import time
 
 gpio.setmode(gpio.BCM)
-gpio.setmode(18, gpio.OUT)
-gpio.setmode(23, gpio.OUT)
+gpio.setup(18, gpio.OUT)
+gpio.setup(23, gpio.OUT)
 
 color = input ("which color should be on, yellow or green")
 if color == "yellow":
