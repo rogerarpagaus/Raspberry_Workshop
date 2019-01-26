42a0dda481c8612fd5d5a3415fd63faff4fe8dc6
Roger Arpagaus <rogerarpagaus@ymail.com>
2019-01-26 16:58:04 +0100

    new example code for the use of a button in combination with a LED.

---
+++ Button-LED.py
@@ -0,0 +1,14 @@
+# Taster Code
+import RPi.GPIO as gpio
+import time
+
+gpio.setmode(gpio.BCM)
+gpio.setup(23,gpio.IN,pull_up_down=gpio.PUD_UP)
+gpio.setup(18,gpio.OUT)
+
+while True:
+  buttonPressed = gpio.input(23)
+  if buttonPressed == 1:
+    gpio.output(18,gpio.LOW)
+  if buttonPressed ==0:
+    gpio.output(18,glio.HIGH)
