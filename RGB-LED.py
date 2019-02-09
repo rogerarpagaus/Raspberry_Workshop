581fa51169756fa3a82fc5a8ef8cf095410478c9
Roger Arpagaus <rogerarpagaus@ymail.com>
2019-02-09 18:09:33 +0100

    Screen Anzeige

--- RGB-LED.py
+++ RGB-LED.py
@@ -13,5 +13,6 @@ gpio.setup(ledPin[2], gpio.OUT)
 while True:
   zufallsPin = random.randint(0,2)
   gpio.output(ledPin[zufallsPin], gpio.HIGH)
+  print (random.randint)
   time.sleep(1)
   gpio.output(ledPin[zufallsPin], gpio.LOW)
