d8badf453a1b34476db43fa5688831374d6fe6e2
Roger Arpagaus <rogerarpagaus@ymail.com>
2019-02-09 17:54:09 +0100

    Initial Code

---
+++ RGP-LED.py
@@ -0,0 +1,17 @@
+# RGB LED for Raspberry Pi B+ in build in case
+
+
+import RPi.GPIO as gpio
+import time
+import random
+
+gpio.setmode(gpio.BCM)
+ledPin = [18,23,24]
+gpio.setup(ledPin[0], gpio.OUT)
+gpio.setup(ledPin[1], gpio.OUT)
+gpio.setup(ledPin[2], gpio.OUT)
+while True:
+  zufallsPin = random.randint(0,2)
+  gpio.output(ledPin[zufallsPin], gpio.HIGH)
+  time.sleep(1)
+  gpio.output(ledPin[zufallsPin], gpio.LOW)
